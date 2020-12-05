1d5475accdd73644e169af10892a9110df58b661
elihenderson33 <71565369+elihenderson33@users.noreply.github.com>
2020-12-04 20:31:23 -0600

    Update JavaCode.js

--- JavaCode.js
+++ JavaCode.js
@@ -1,65 +1,78 @@
-var racerArray = [
-    {
-        name: "Blue",
-        source: "JoeBiden.png",
-        raceSpeed: 0
-    },
-    {
-        name: "Red",
-        source: "DonaldTrump.png",
-        raceSpeed: 0
-    }
-]
+$(function(){
+    let vm = new Vue({
+       el: '#exciteBike',
+      data: {
+       racing: false,
+       winner: null,
+       redBike: 0,
+       blueBike: 0,
+       tick: 0,
+       intervall: null,
+        },
+       methods: {
 
-function displayRacers() {
-    document.getElementById("racer1pic").src = racerArray[0].source;
-    document.getElementById("racer2pic").src = racerArray[1].source;
-}
+           go() {
+               if (this.winner) {
+                   this.reset()
+                   return
+               }
+               this.racing = true
+               this.interval = setInterval(() => {
+                   this.moveRider()
 
-function startRace() {
-    //var active = false;
-    var stoplight = document.getElementById("stoplight");
-    stoplight.src = "GreenFlag.png";
-    var stoplight2 = document.getElementById("stoplight2");
+               }, 85)
+           },
+           moveRider() {
+               this.tick++
+               this.redBike += (Math.random() >= .4) ? 2 : 1
+               this.blueBike += (Math.random() >= .4) ? 2 : 1
+               this.decision()
+           },
+           decision() {
+               if (this.redBike == this.blueBike) return
 
-    var distance = 1300;
-    var racer1Image = document.getElementById("racer1");
-    var racer2Image = document.getElementById("racer2");
+               if (this.redBike >= 87) {
+                   this.showWin('redBike')
+               }
 
-        var frames = setInterval(movement, 20);
-        function movement()
-        {
-        
-            if (racerArray[0].racerSpeed < distance || racerArray[1].raceSpeed < distance)
-            {
-                racerArray[0].raceSpeed += Math.floor(Math.random() * 11);
-                racerArray[1].raceSpeed += Math.floor(Math.random() * 11);
-      
-                racer1Image.style.left = racerArray[0].raceSpeed + "px";
-                racer2Image.style.left = racerArray[1].raceSpeed + "px";
+               if (this.blueBike >= 87) {
+                   this.showWin('blueBike')
+               }
+           },
+           showWin(rider) {
+               clearInterval(this.interval)
+               this.interval = null
+               this.racing = false
+               this.winner = rider
+           },
+           reset() {
+               this.racing = false
+               this.winner = null
+               this.redBike = 0
+               this.blueBike = 0
+               this.tick = 0
+           }
+       },
+       computed: {
 
-                if (racerArray[0].raceSpeed >= distance)
-                {
-                    stoplight2.src = racerArray[0].source;
-                    stoplight2.width = 300;
-                    clearInterval(frames);
-                }
-                if (racerArray[1].raceSpeed >= distance)
-                {
-                    stoplight2.src = racerArray[1].source;
-                    stoplight2.width = 300;
-                    clearInterval(frames);
-                }
-
-                stoplight2.addEventListener("click", function() 
-                {
-                var racer1Image = document.getElementById("racer1");
-                 var racer2Image = document.getElementById("racer2");
-                 racer1Image.style.left = 0 + "px";
-                 racer2Image.style.left = 0;
-
-                 stoplight.src = "RedFlag.png";
-                })
-        }
-    }
-}
+           redStyles() {
+               return {
+                   left: `${this.redBike}vw`
+               }
+           },
+           redClass() {
+               if (!this.winner) return
+               return this.winner == 'redBike' ? 'animated flash infinite winner' : 'animated zoomOutRight'
+           },
+           blueStyles() {
+               return {
+                   left: `${this.blueBike}vw`
+               }
+           },
+           blueClass() {
+               if (!this.winner) return
+               return this.winner == 'blueBike' ? 'animated flash infinite winner' : 'animated zoomOutRight'
+           }
+       }
+    })
+})

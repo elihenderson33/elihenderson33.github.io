2a05b58ad193bb75f84fdfa282328abf2d62f2bc
elihenderson33 <71565369+elihenderson33@users.noreply.github.com>
2020-12-04 22:06:56 -0600

    Update JavaCode.js

--- JavaCode.js
+++ JavaCode.js
@@ -1,58 +1,94 @@
-let app = new Vue({
-    el: '#race',
+var vue = new Vue({
+    el: "#app",
     data: {
+        race: true,
         racing: false,
-        winner: null,
-        blossom: 0,
-        buttercup: 0,
-        tick: 0,
-        interval: null,            
-    },
-    computed: {
-        winning() {
-            return this.blossom > this.buttercup ? this.blossom : this.buttercup
-        },            
-        blossomStyle() {
-            return {
-                left: `${this.blossom}vw`
-            }             
-        },
-        buttercupStyle() {
-            return {
-                left: `${this.buttercup}vw`
-            }
-        },
-
+        winner: 0,
+        p1Move : 0,
+        p2Move : 0,
+        interval: null,
+        moving: true,
+        awesomeDude: 0
     },
     methods: {
         startRace() {
-            this.racing = true
-            this.interval = setInterval(() => {
-                this.progressPlayers()
-            }, 50);
-        },
-        progressPlayers() {
-            this.tick++
-            this.blossom += Math.random() > .5 ? 1 : 0   //"roll dice"
-            this.buttercup += Math.random() > .5 ? 1 : 0 //"roll dice"
-            this.checkWinnah()
-        },
-        checkWinnah(){
-            if (this.blossom == this.buttercup) return
-
-            if (this.blossom >= 85){
-                blossom.winner=true;
-                buttercup.winner=false;
-                this.winnahWinnah()
+            if (this.race) {
+                this.race = false;
+                this.awesomeDude = 0;
+                this.racing = true;
+                this.moving = true;
+                this.p1Move = 0;
+                this.p2Move = 0;
+                this.interval = setInterval(this.move, 200);
             }
-            if (this.buttercup >= 85) {
-                blossom.winner=false;
-                buttercup.winner=true;
-                this.winnahWinnah()
+            else return;
+        } ,
+        move() {
+            this.checkWinner();
+            if (this.moving) {
+                this.p1Move += Math.floor(Math.random() * 10);
+                this.p2Move += Math.floor(Math.random() * 10);
+            }
+        } ,
+        checkWinner() {
+            if (this.p1Move == this.p2Move) return;
+            else if (this.p1Move >= 85 && this.p1Move > this.p2Move) {
+                clearInterval(this.interval);
+                this.player1Win();
+                this.moving = false;
+                this.race = true;
+            }
+            else if (this.p2Move >= 85 && this.p2Move > this.p1Move) {
+                clearInterval(this.interval);
+                this.player2Win();
+                this.moving = false;
+                this.race = true;
+            }
+        } ,
+        player1Win() {
+            this.awesomeDude = 1;
+            this.racing = false;
+        } ,
+        player2Win() {
+            this.awesomeDude = 2;
+            this.racing = false;
+        }
+    },
+    computed: {
+        buttonImage() {
+            return this.racing ? "RedFlag.png" : "GreenFlag.png";
+        } ,
+        winnerImage() {
+            if (this.awesomeDude == 1)
+                return "JoeBiden.png";
+            else if (this.awesomeDude == 2)
+                return "DonaldTrump.png";
+            else {
+                return "tie.png";
             }
         },
-        winnahWinnah(){
-            clearInterval(this.interval);
+        playerImage() {
+            if (this.awesomeDude == 1)
+                return "BidenWins.png";
+            else if (this.awesomeDude == 2)
+                return "TrumpWins.png";
+            else {
+                return "tie.png";
+            }
+        },
+        player1Position() {
+            return {
+                left: this.p1Move + "vw",
+                height: "200px",
+                width: "200px"
+            }
+        } ,
+        player2Position() {
+            return {
+                left: this.p2Move + "vw",
+                height: "200px",
+                width: "200px"
+            }
         }
     }
 })

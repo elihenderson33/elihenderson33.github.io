c8e9cca58ab8b7211e0e4e246f2fef8dbc6692cd
elihenderson33 <71565369+elihenderson33@users.noreply.github.com>
2020-12-04 21:03:28 -0600

    Update JavaCode.js

--- JavaCode.js
+++ JavaCode.js
@@ -1,78 +1,58 @@
-$(function(){
-    let vm = new Vue({
-       el: '#exciteBike',
-      data: {
-       racing: false,
-       winner: null,
-       redBike: 0,
-       blueBike: 0,
-       tick: 0,
-       intervall: null,
+let app = new Vue({
+    el: '#race',
+    data: {
+        racing: false,
+        winner: null,
+        blossom: 0,
+        buttercup: 0,
+        tick: 0,
+        interval: null,            
+    },
+    computed: {
+        winning() {
+            return this.blossom > this.buttercup ? this.blossom : this.buttercup
+        },            
+        blossomStyle() {
+            return {
+                left: `${this.blossom}vw`
+            }             
         },
-       methods: {
+        buttercupStyle() {
+            return {
+                left: `${this.buttercup}vw`
+            }
+        },
 
-           go() {
-               if (this.winner) {
-                   this.reset()
-                   return
-               }
-               this.racing = true
-               this.interval = setInterval(() => {
-                   this.moveRider()
+    },
+    methods: {
+        startRace() {
+            this.racing = true
+            this.interval = setInterval(() => {
+                this.progressPlayers()
+            }, 50);
+        },
+        progressPlayers() {
+            this.tick++
+            this.blossom += Math.random() > .5 ? 1 : 0   //"roll dice"
+            this.buttercup += Math.random() > .5 ? 1 : 0 //"roll dice"
+            this.checkWinnah()
+        },
+        checkWinnah(){
+            if (this.blossom == this.buttercup) return
 
-               }, 85)
-           },
-           moveRider() {
-               this.tick++
-               this.redBike += (Math.random() >= .4) ? 2 : 1
-               this.blueBike += (Math.random() >= .4) ? 2 : 1
-               this.decision()
-           },
-           decision() {
-               if (this.redBike == this.blueBike) return
-
-               if (this.redBike >= 87) {
-                   this.showWin('redBike')
-               }
-
-               if (this.blueBike >= 87) {
-                   this.showWin('blueBike')
-               }
-           },
-           showWin(rider) {
-               clearInterval(this.interval)
-               this.interval = null
-               this.racing = false
-               this.winner = rider
-           },
-           reset() {
-               this.racing = false
-               this.winner = null
-               this.redBike = 0
-               this.blueBike = 0
-               this.tick = 0
-           }
-       },
-       computed: {
-
-           redStyles() {
-               return {
-                   left: `${this.redBike}vw`
-               }
-           },
-           redClass() {
-               if (!this.winner) return
-               return this.winner == 'redBike' ? 'animated flash infinite winner' : 'animated zoomOutRight'
-           },
-           blueStyles() {
-               return {
-                   left: `${this.blueBike}vw`
-               }
-           },
-           blueClass() {
-               if (!this.winner) return
-               return this.winner == 'blueBike' ? 'animated flash infinite winner' : 'animated zoomOutRight'
-           }
-       }
-    })
+            if (this.blossom >= 85){
+                blossom.winner=true;
+                buttercup.winner=false;
+                this.winnahWinnah()
+            }
+            if (this.buttercup >= 85) {
+                blossom.winner=false;
+                buttercup.winner=true;
+                this.winnahWinnah()
+            }
+        },
+        winnahWinnah(){
+            clearInterval(this.interval);
+        }
+    }
 })
